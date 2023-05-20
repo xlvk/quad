29981085f5d1aad423aeaf4a418d9f3c9c9a963b
Kaloshib <aadaoy@gmail.com>
2023-05-20 19:10:32 +0300

    a

---
+++ QuadA.go
@@ -0,0 +1,58 @@
+package piscine
+
+import (
+	"github.com/01-edu/z01"
+)
+
+func QuadA(x, y int) {
+	if x <= 0 || y <= 0 {
+		return
+	}
+	i := 0
+	if i < x {
+		for c := 0; c < 1; c++ {
+			z01.PrintRune('o')
+		}
+		for c := 1; c < x-1; c++ {
+			z01.PrintRune('-')
+		}
+		for c := x; c <= x; c++ {
+			if x > 1 {
+				z01.PrintRune('o')
+			} else {
+				break
+			}
+		}
+	}
+	z01.PrintRune('\n')
+	for i := 0; i < y-2; i++ {
+		z01.PrintRune('|')
+		if x == 1 {
+			z01.PrintRune('\n')
+		}
+		if y >= 2 {
+			for n := 0; n < x-2; n++ {
+				z01.PrintRune(' ')
+			}
+			if x >= 2 {
+				z01.PrintRune('|')
+				z01.PrintRune('\n')
+			}
+		}
+	}
+	if y >= 2 {
+		for c := 0; c < 1; c++ {
+			z01.PrintRune('o')
+		}
+		for c := 1; c < x-1; c++ {
+			z01.PrintRune('-')
+		}
+		for c := x; c <= x; c++ {
+			if x > 1 {
+				z01.PrintRune('o')
+			} else {
+				break
+			}
+		}
+	}
+}
